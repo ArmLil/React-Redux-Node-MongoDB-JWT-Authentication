dbc5e0da04a233521c7bbabba782030249e13fd1
ArmLil <armlilhov@mail.ru>
2019-01-09 16:51:20 +0300

    Check the logged in user status

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -40,10 +40,19 @@ class Register extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
+        if(nextProps.auth.isAuthenticated) {
+            this.props.history.push('/')
+        }
         if(nextProps.errors) {
             this.setState({
                 errors: nextProps.errors
             });
+        }
+    }
+
+    componentDidMount() {
+        if(this.props.auth.isAuthenticated) {
+            this.props.history.push('/');
         }
     }
 
@@ -118,9 +127,11 @@ class Register extends Component {
 
 Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
+    auth: state.auth,
     errors: state.errors
 });
 
